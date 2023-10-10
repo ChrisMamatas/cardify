66d2ac6e6b9ddf6a2df15b45e40073b2518f6870
Tyler St. Amand <tyler.amand@logisticsplus.com>
2023-10-10 19:16:49 -0400

    Added bootstrap, added card view, card create in progress

---
+++ src/Components/Card.tsx
@@ -0,0 +1,39 @@
+// src/CardBox.tsx
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+interface CardProps {
+    title: string;
+    description: string;
+    imageUrl: string;
+    buttonText?: string;
+    onButtonClick?: () => void;
+}
+
+
+
+const CardComponent: React.FC<CardProps> = ({
+    title,
+    description,
+    imageUrl,
+    buttonText,
+    onButtonClick,
+}) => {
+    return (
+        <Card style={{ width: '18rem', margin: '16px' }}>
+            <Card.Img variant="top" src={imageUrl} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{description}</Card.Text>
+                {buttonText && (
+                    <Button variant="primary" onClick={onButtonClick}>
+                        {buttonText}
+                    </Button>
+                )}
+            </Card.Body>
+        </Card>
+    );
+};
+
+
+export default CardComponent;
--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -1,8 +1,89 @@
-export default function Cards() {
+import { Route, Routes, useHref } from 'react-router-dom';
+import CardComponent from '../Components/Card';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import CreateCard from './CreateCard';
 
+const cardData = [
+    {
+        title: "Card 1",
+        description: "This is the description for card 1.",
+        imageUrl: "https://via.placeholder.com/150",
+        buttonText: "More Info",
+    },
+    {
+        title: "Card 2",
+        description: "This is the description for card 2.",
+        imageUrl: "https://via.placeholder.com/150",
+        buttonText: "Details",
+    },
+    {
+        title: "Card 3",
+        description: "This is the description for card 3.",
+        imageUrl: "https://via.placeholder.com/150",
+        buttonText: "Learn More",
+    },
+    {
+        title: "Card 4",
+        description: "This is the description for card 4.",
+        imageUrl: "https://via.placeholder.com/150",
+        buttonText: "Click Me",
+    },
+    {
+        title: "Card 5",
+        description: "This is the description for card 5.",
+        imageUrl: "https://via.placeholder.com/150",
+        buttonText: "More Info",
+    },
+    {
+        title: "Card 6",
+        description: "This is the description for card 6.",
+        imageUrl: "https://via.placeholder.com/150",
+        buttonText: "Details",
+    },
+    {
+        title: "Card 7",
+        description: "This is the description for card 7.",
+        imageUrl: "https://via.placeholder.com/150",
+        buttonText: "Learn More",
+    },
+    {
+        title: "Card 8",
+        description: "This is the description for card 8.",
+        imageUrl: "https://via.placeholder.com/150",
+        buttonText: "Click Me",
+    },
+];
+
+export default function Cards() {
     return (
-        <h1>
-            Cards
-        </h1>
-    )
-}
+        <><Container style={{ marginTop: "32px" }}>
+            <Row md={12}>
+                {cardData.map((card, index) => (
+                    <Col md={3}>
+                        <CardComponent
+                            key={index}
+                            title={card.title}
+                            description={card.description}
+                            imageUrl={card.imageUrl}
+                            buttonText={card.buttonText}
+                            onButtonClick={() => alert(`${card.title} clicked!`)} />
+                    </Col>
+                ))}
+            </Row>
+            <Row style={{ marginTop: "32px" }}>
+                <Col md={6}>
+                    <Button variant="primary" size="lg" style={{ float: 'left', width: '100%' }} href="CreateCard">Create Card
+
+                    </Button>
+                </Col>
+                <Col md={6} className='justify-content-md-end'>
+                    <Button variant="primary" size="lg" style={{ float: 'right', width: '100%' }}>Delete Card</Button>
+                </Col>
+            </Row>
+
+        </Container>
+            <Routes>
+                <Route path={"CreateCard"} element={<CreateCard />} />
+            </Routes></>
+    );
+};
