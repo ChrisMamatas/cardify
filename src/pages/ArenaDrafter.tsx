2b87469c28b484d5c06973bbcf33cf901a971cc7
Carlo_Munizza <cam7134@psu.edu>
2023-10-24 23:45:59 -0400

    nice looking code
    make the drafter page not 1000 lines long by using the map functionality to generate rows and columns for the cards, also messed with css

--- src/pages/ArenaDrafter.tsx
+++ src/pages/ArenaDrafter.tsx
@@ -5,6 +5,35 @@ import {FiBookmark} from "react-icons/fi";
 
 export default function ArenaDrafter() {
     const now = 60;
+    const record = [
+        { card: 1 },
+        { card: 2 },
+        { card: 3 },
+        { card: 4 },
+        { card: 5 },
+        { card: 6 },
+        { card: 7 },
+        { card: 8 },
+    ];
+
+    const table = [
+        { record: 1 },
+        { record: 2 },
+        { record: 3 },
+        { record: 4 },
+    ];
+
+    const players = [
+        {player: 1},
+        {player: 2},
+        {player: 3},
+        {player: 4},
+        {player: 5},
+        {player: 6},
+        {player: 7},
+        {player: 8},
+    ];
+
     return (
         <div>
             <h1 className="center">
@@ -14,324 +43,48 @@ export default function ArenaDrafter() {
             <Container>
                 <Row>
                     <Col>
-                        <Row className={"m-4"}>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                    <div className="flex-nowrap">
-                                        <button className="custom" style={{borderBottomLeftRadius:"50%"}}>
-                                            <FiBookmark/>
-                                        </button>
-                                        <button className="custom" style={{borderBottomRightRadius:"50%"}}>
-                                            Draft
-                                        </button>
-                                    </div>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row className={"m-4"}>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row className={"m-4"}>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row className={"m-4"}>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                            <Col className="d-flex">
-                                <div className="browse justify-content-center">
-                                    <PreviewCard height="90px"/>
-                                </div>
-                            </Col>
-                        </Row>
+                        {table.map(item =>(
+                            <Row key ={item.record} className={"m-4"}>
+                                {record.map(item =>(
+                                    <Col key ={item.card} className="d-flex">
+                                        <div className="browse justify-content-center">
+                                            <PreviewCard height="90px"/>
+                                            <div className="flex-nowrap button-container">
+                                                <button className="customBookmark">
+                                                    <FiBookmark/>
+                                                </button>
+                                                <button className="customDraft">
+                                                    Draft
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                ))}
+                            </Row>
+                        ))}
                     </Col>
-                    <Col md={{span: 1}}>
-                        <Row>
-                            Name1
-                            <div className="d-flex">
-                                <span>
-                                    <PreviewCard  height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                            </div>
-
-                        </Row>
-                        <Row >
-                            Name2
-                            <div className="d-flex">
-                                <span>
-                                    <PreviewCard  height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                            </div>
-                        </Row>
-                        <Row>
-                            Name3
-                            <div className="d-flex">
-                                <span>
-                                    <PreviewCard  height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                            </div>
-                        </Row>
-                        <Row>
-                            Name4
-                            <div className="d-flex">
-                                <span>
-                                    <PreviewCard  height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                            </div>
-                        </Row>
-                        <Row>
-                            Name5
-                            <div className="d-flex">
-                                <span>
-                                    <PreviewCard  height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                            </div>
-                        </Row>
-                        <Row>
-                            Name6
-                            <div className="d-flex">
-                                <span>
-                                    <PreviewCard  height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                            </div>
-                        </Row>
-                        <Row>
-                            Name7
-                            <div className="d-flex">
-                                <span>
-                                    <PreviewCard  height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                            </div>
-                        </Row>
-                        <Row>
-                            Name8
-                            <div className="d-flex">
-                                <span>
-                                    <PreviewCard  height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                                <span>
-                                    <PreviewCard height="30px"/>
-                                </span>
-                            </div>
-                        </Row>
+                    <Col  md={{span: 1}}>
+                        {players.map(item => (
+                            <Row key = {item.player}>
+                                Player
+                                <div className="d-flex">
+                                    <span>
+                                        <PreviewCard  height="30px"/>
+                                    </span>
+                                    <span>
+                                        <PreviewCard height="30px"/>
+                                    </span>
+                                    <span>
+                                        <PreviewCard height="30px"/>
+                                    </span>
+                                    <span>
+                                        <PreviewCard height="30px"/>
+                                    </span>
+                                </div>
+                            </Row>
+                        ))}
                     </Col>
                 </Row>
-
                 <Row>
                     <div>
                         <ProgressBar animated now={now} label={`${now} sec`}/>
